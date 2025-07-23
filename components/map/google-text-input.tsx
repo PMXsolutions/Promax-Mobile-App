17c5b45a6eb7ca137b846bcfffb9609d96e05ae7
MakindeTobby <makinde.tobiloba@gmail.com>
2025-07-23 20:56:34 +0100

    chore: google-input fixed

--- components/map/google-text-input.tsx
+++ components/map/google-text-input.tsx
@@ -27,10 +27,12 @@ const GoogleTextInput = ({
         containerStyle,
       ]}
     >
-      <GooglePlacesAutocomplete
+      {/* <GooglePlacesAutocomplete
         fetchDetails={true}
         placeholder="Search"
         debounce={200}
+        minLength={2}
+        timeout={1000}
         styles={{
           textInputContainer: {
             alignItems: "center",
@@ -96,6 +98,90 @@ const GoogleTextInput = ({
             }}
           >
             <Feather name={"arrow-left"} size={24} />
+          </TouchableOpacity>
+        )}
+        textInputProps={{
+          placeholderTextColor: "gray",
+          placeholder: initialLocation ?? "Type in an address...",
+        }}
+      /> */}
+      <GooglePlacesAutocomplete
+        fetchDetails={true}
+        placeholder="Search"
+        debounce={200}
+        minLength={2}
+        timeout={10000}
+        enablePoweredByContainer={false}
+        nearbyPlacesAPI="GooglePlacesSearch"
+        currentLocation={false}
+        predefinedPlaces={[]} // Prevents crash
+        predefinedPlacesAlwaysVisible={false}
+        listUnderlayColor="#eee"
+        isRowScrollable={true}
+        keyboardShouldPersistTaps="handled"
+        onFail={(err) => console.warn("Autocomplete failed:", err)}
+        onNotFound={() => console.log("No results found")}
+        styles={{
+          textInputContainer: {
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: 20,
+            marginHorizontal: 10,
+            position: "relative",
+            shadowColor: "#d4d4d4",
+          },
+          textInput: {
+            backgroundColor: textInputBackgroundColor ?? "white",
+            fontSize: 16,
+            fontWeight: "600",
+            marginTop: 5,
+            width: "100%",
+            borderRadius: 200,
+          },
+          listView: {
+            backgroundColor: textInputBackgroundColor ?? "white",
+            position: "relative",
+            top: 0,
+            width: "100%",
+            borderRadius: 10,
+            shadowColor: "#d4d4d4",
+            zIndex: 99,
+          },
+        }}
+        onPress={(data, details = null) => {
+          handlePress({
+            latitude: details?.geometry.location.lat!,
+            longitude: details?.geometry.location.lng!,
+            address: data.description,
+          });
+        }}
+        query={{
+          key: GOOGLE_MAPS_API_KEY,
+          language: "en",
+        }}
+        renderRightButton={() => (
+          <View
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              width: 24,
+              height: 24,
+            }}
+          >
+            <Feather name={icon || "search"} size={24} />
+          </View>
+        )}
+        renderLeftButton={() => (
+          <TouchableOpacity
+            onPress={() => router.back()}
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              width: 24,
+              height: 24,
+            }}
+          >
+            <Feather name="arrow-left" size={24} />
           </TouchableOpacity>
         )}
         textInputProps={{
